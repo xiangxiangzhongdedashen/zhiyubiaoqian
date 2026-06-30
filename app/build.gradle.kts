ecc2e85bc15b90187af0103e5666d3d592c21139
xiangxiangzhongdedashen <xiangxiangzhongdedashen@outlook.com>
2026-06-30 09:32:48 +0800

    v1.11: 更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.example.myziyubiaoqian"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.1"
+        versionCode = 21
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
